a595f6fd578b2dc764e935b22137b0673155e738
No2Pou <tlitgol@foxmail.com>
2017-05-13 15:15:39 +0800

    第三次实验

---
+++ LearnWork/Assets/scripts/BackgroundParallax.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+	public Transform[] backgrounds;
+	public float parallaxScale;
+	public float parallaxReductionFactor;
+	public float smoothing;
+
+	private Transform cam;
+	private Vector3 previousCampos;//摄像机上一帧的位置
+
+	void Awake()
+	{
+		cam = Camera.main.transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		previousCampos = cam.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float parallax = (previousCampos.x - cam.position.x) * parallaxScale;
+
+		for (int i = 0; i < backgrounds.Length; i++) 
+		{
+			float backgroundTargetPosX = backgrounds [i].position.x + parallax * (i * parallaxReductionFactor + 1);
+
+			Vector2 backgroundTargetPos = new Vector2 (backgroundTargetPosX, backgrounds [i].position.y);
+		
+			backgrounds [i].position = Vector2.Lerp (backgrounds [i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+		}
+		previousCampos = cam.position;
+	}
+}
--- LearnWork/Assets/scripts/CameraFollow.cs
+++ LearnWork/Assets/scripts/CameraFollow.cs
@@ -2,31 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFallow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour {
 
 	// Use this for initialization
 	public Vector2 XY_Max;
 	public Vector2 XY_Min;
+
+	public float Xdistance=1f;
+	public float Ydistance=1f;
+	public float xSmooth = 8f;
+	public float ySmooth = 8f;
+
 	public Transform heropos;
-	public float Xdistance=1;
-	public float Ydistance=2;
+
 	void Start () {
 		heropos = GameObject.FindWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float fnewX = heropos.position.x;
-		float fnewY = heropos.position.y;
+
+	}
+
+	void FixedUpdate()
+	{
+		float fnewX = transform.position.x;
+		float fnewY = transform.position.y;
 		if(IsLargX())
-			fnewX = Mathf.Clamp(heropos.position.x,XY_Min.x,XY_Max.x);
+			fnewX = Mathf.Lerp(transform.position.x, heropos.position.x,xSmooth * Time.deltaTime);
 		if(IsLargY())
-			fnewY = Mathf.Clamp(heropos.position.y,XY_Min.y,XY_Max.y);
+			fnewY = Mathf.Lerp(transform.position.y, heropos.position.y,ySmooth * Time.deltaTime);
+
+		fnewX = Mathf.Clamp (fnewX, XY_Min.x, XY_Max.x);
+		fnewY = Mathf.Clamp (fnewY, XY_Min.y, XY_Max.y);
 
 		transform.position = new Vector3 (fnewX,
-										  fnewY,
-										  transform.position.z);
+			fnewY,
+			transform.position.z);	
 	}
+
 	private bool IsLargX()
 	{
 		return(Mathf.Abs (heropos.position.x - transform.position.x) > Xdistance);
